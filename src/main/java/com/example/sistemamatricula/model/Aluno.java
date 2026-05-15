d146cddb5aea5906175385ac0b67f3820aa011c6
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-15 16:42:59 -0300

    Update Aluno.java

--- src/main/java/com/example/sistemamatricula/model/Aluno.java
+++ src/main/java/com/example/sistemamatricula/model/Aluno.java
@@ -1,4 +1,43 @@
 package com.example.sistemamatricula.model;
+import java.time.LocalDate;
 
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCompleto;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private LocalDate nascimento
+    
+     public Aluno(String nomeCompleto, String cpf, String email, String telefone, LocalDate nascimento){
+        this.nomeCompleto = nomeCompleto;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.nascimento = nascimento;}
+        
+    public String getnomeCompleto {
+        return nomeCompleto;
+    }
+    
+    public String getcpf {
+        return cpf
+    }
+    
+    public String getemail {
+        return email;
+    }
+    
+    public String gettelefone {
+        return telefone
+    }
+    
+    public LocalDate getnascimento{
+        return nascimento
+    }
+
 }

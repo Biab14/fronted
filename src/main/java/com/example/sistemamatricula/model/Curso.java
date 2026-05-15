8df121a076f7cb54af40a3b3a5fe065b12bf34fe
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-15 18:08:05 -0300

    Update Curso.java

--- src/main/java/com/example/sistemamatricula/model/Curso.java
+++ src/main/java/com/example/sistemamatricula/model/Curso.java
@@ -1,4 +1,20 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCurso;
+    private String codigo;
+    private String horario;
+    private int vagas;
+    private int vagasOcupadas;
 }

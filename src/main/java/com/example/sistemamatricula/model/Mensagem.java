9e91641a7b79ef18930eb474540e1409dcd2b63b
William Santos Bitencourt de Sousa <165169533+WilliamCutelo@users.noreply.github.com>
2026-05-16 09:34:02 -0300

    Update Mensagem.java

--- src/main/java/com/example/sistemamatricula/model/Mensagem.java
+++ src/main/java/com/example/sistemamatricula/model/Mensagem.java
@@ -1,4 +1,31 @@
-package com.example.sistemamatricula.model;
+package com.example.sistemamatricula.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mensagem")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Mensagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String mensagem;
+
+	private LocalDateTime data
+
+	@ManyToOne
+	@JoinColumn(name = "id_atendente")
+	private Atendente atendente;
+
+	@ManyToOne
+	@JoinColumn(name = "id_aluno")
+	private Aluno aluno;
 }
